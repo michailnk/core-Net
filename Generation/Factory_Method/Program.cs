be4a767b237e86ad3d1b3c97086fc6a194d4c599
Михаил <meekesp@gmail.com>
2020-07-23 14:18:22 +0300

    add factory mathod

---
+++ Generation/Factory_Method/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+/// <summary>
+///  this is the basic pattern for generating templates. Its task is to describe not only
+///  the generation of objects, but also to describe the interactions between the base and 
+///  the derived. This is a fundamental pattern.
+///  Its main purpose is to be the basis for the other 4 generative patterns
+/// </summary>
+namespace Factory_Method {
+
+    abstract class Product { }
+    abstract class Creator {
+        Product product;
+        public abstract Product FactoryMethod();
+        public void AnOnperation() {
+            product = FactoryMethod();
+        }
+    }
+
+    class ConcretProduct : Product {
+        public ConcretProduct() {
+            Console.WriteLine(this.GetHashCode());
+        }
+    }
+    class ConcreteCreator : Creator {
+        public override Product FactoryMethod() {
+            return new ConcretProduct();
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            Creator creator = null;
+            Product product = null;
+
+            creator = new ConcreteCreator();
+            product = creator.FactoryMethod();
+
+            creator.AnOnperation();
+        }
+    }
+}
---
+++ Generation/VM_Constuct/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace VM_Constuct {
+
+    abstract class Product { }
+    class DefProduct : Product { }
+    class SpecialProduct : Product { }
+
+    abstract class Creator {
+        protected Product product = null;
+
+        protected Creator() {
+            product = FactoryMethod();
+            //Console.WriteLine(product.GetType().Name);
+        }
+
+        public virtual Product FactoryMethod() {
+            return new DefProduct();
+        }
+    }
+
+    class ConcreteCreator : Creator {
+        public ConcreteCreator() {
+            product = new SpecialProduct();
+            //Console.WriteLine(product.GetType().Name);
+        }
+        public new Product FactoryMethod() {
+            return new SpecialProduct();
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+
+            Creator creator = new ConcreteCreator();
+
+            Console.WriteLine( creator.FactoryMethod().GetType().Name);
+            Console.WriteLine(((ConcreteCreator)creator).FactoryMethod().GetType().Name);
+        }
+    }
+}
