9c8a81e86d3b6a0be369a597257023fc8431150b
romn <romn@DESKTOP-45B6LC7>
2020-09-25 18:17:38 +0300

    load decorator

---
+++ StructuralPatterns/Decorator/Decorator_01/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Decorator_01 {
+    class Program {
+        static void Main(string[] args) {
+            Component component = new ConcreteComponent();
+            Decorator decoratorA = new ConcreteDecoratorA();
+            Decorator decoratorB = new ConcreteDecoratorB();
+
+            decoratorA.Component = component;
+            decoratorB.Component = decoratorA;
+            decoratorB.Operation();
+        }
+    }
+
+    abstract class Component { public abstract void Operation(); }
+
+    class ConcreteComponent:Component {
+        public override void Operation() {
+            Console.WriteLine("ConcreteComponent");
+        }
+    }
+    abstract class Decorator:Component {
+        public Component Component { protected get; set; }
+        public override void Operation() {
+            if(Component != null)
+                Component.Operation();
+        }
+    }
+
+    class ConcreteDecoratorA:Decorator {
+        string addState = "Some State";
+        public override void Operation() {
+            base.Operation();
+            Console.WriteLine(addState);
+        }
+    }
+
+    class ConcreteDecoratorB:Decorator {
+        void AddBehavior() {
+            Console.WriteLine("Behavior");
+        }
+
+        public override void Operation() {
+            base.Operation();
+            AddBehavior();
+        }
+    }
+}
