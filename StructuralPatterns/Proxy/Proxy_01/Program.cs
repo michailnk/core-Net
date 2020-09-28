1d914f85c988c653a7dba536b15251e8ee9887f5
romn <romn@DESKTOP-45B6LC7>
2020-09-28 13:59:50 +0300

    load proxy

---
+++ StructuralPatterns/Proxy/Proxy_01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+/*  proxy hase 4 types
+ * 1) удаленный заместитель (посол, ambassador) (WCF) proxy может являтся локальным предстовителем 
+ *     в другом адрессном пространсве
+ * 2) виртуальный заместитель. Создает тяжелые объекты по требованию (подгузка изображений размытая, а затем детальная)
+ * 3) защищающий заместитель. Контролирует доступ к объекту, когда для разных
+ *     объетов определены различные права доступа.(CRUD)
+ * 4) умная ссылка (указатель) в С++ подсчет колличество ссылок на экземпляр. Конструкция lock, 
+ *     переменная значения LINQ - умная ссылка, оператор yield, async await(машина асинхронных состояний)
+ */
+namespace Proxy_01 {
+    class Program {
+        static void Main(string[] args) {
+            IHuman Bruce = new Operator();
+            IHuman surrogate = new Surrogate(Bruce);
+            surrogate.Request();        
+        }
+    }
+
+    interface IHuman {
+        void Request();
+    }
+
+    class Operator:IHuman {
+        public void Request() {
+            Console.WriteLine("Operator");
+        }
+    }
+
+    class Surrogate:IHuman {
+        IHuman @operator;
+
+        public Surrogate(IHuman @operator) {
+            this.@operator = @operator;
+        }
+
+        public void Request() {
+            this.@operator?.Request();
+        }
+    }
+}
---
+++ StructuralPatterns/Proxy/Proxy_02(GRUD)/Pattern/Subject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proxy_02_GRUD_.Pattern {
+    abstract class Subject {
+        public abstract void Create(string key, string value);
+        public abstract string Read(string key);
+        public abstract bool Update(string key, string value);
+        public abstract bool Delete(string key);
+    }
+}
